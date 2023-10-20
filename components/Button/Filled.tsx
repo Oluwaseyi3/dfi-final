19af3b639880846134a106460386388b9cf9c195
Oluwaseyi Joshua <iamoluwaseyiolasupo@gmail.com>
2023-10-20 03:44:05 +0000

    Update 3 files

--- components/Button/Filled.tsx
+++ components/Button/Filled.tsx
@@ -1,25 +1,21 @@
+
 import { Button } from 'antd';
 import styled from 'styled-components';
 
 interface Props {
-    disabled?: boolean;
+  disabled?: boolean;
 }
 
-export default styled(Button)<Props>`
-    background: ${(props) =>
-        props.disabled ? props.theme.spaceGrey : props.theme.primary + ' 0% 0% no-repeat padding-box'};
-    border: ${(props) => '2px solid ' + (props.disabled ? props.theme.grey : props.theme.primary)} !important;
-    border-radius: 15px;
-    color: ${(props) => props.theme.white};
-    padding: 10px 35px;
-    text-align: center;
-    height: 45px;
-    font-weight: bold;
-
-    &:hover {
-        background: ${(props) =>
-            props.disabled ? 'default' : props.theme.primaryDark + ' 0% 0% no-repeat padding-box'};
-        border: ${(props) => '2px solid ' + (props.disabled ? props.theme.grey : props.theme.primaryDark)} !important;
-        color: ${(props) => props.theme.white};
-    }
+const FilledButton = styled(Button)<Props>`
+  background: ${(props) => props.disabled ? props.theme.spaceGrey : props.theme.primary + ' 0% 0% no-repeat padding-box'};
+  border: ${(props) => '2px solid ' + (props.disabled ? props.theme.grey : props.theme.primary)} !important;
+  color: ${(props) => props.disabled ? props.theme.grey : '#fff'} !important;
+  font-size: 1rem;
+  font-weight: bold;
+  padding: 0.5rem 1rem;
+  text-align: center;
+  text-transform: uppercase;
+  width: 100%;
 `;
+
+export default FilledButton;
