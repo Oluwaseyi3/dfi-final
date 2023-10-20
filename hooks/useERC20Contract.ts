35f8b20bb661a7658b5f60b0805dee7ad29ca3ba
Oluwaseyi Joshua <iamoluwaseyiolasupo@gmail.com>
2023-10-20 04:53:00 +0000

    Delete 16 files

--- hooks/useERC20Contract.ts
+++
@@ -1,6 +0,0 @@
-// import useContract from './useContract';
-// import ERC20ABI from '../contracts/ERC20.json';
-
-// export default function useERC20Contract(tokenAddress: string | undefined, withSigner = false) {
-//     return useContract(tokenAddress, ERC20ABI, withSigner);
-// }
