c944cb646facf9188663128dd84d1070524f89ee
Oluwaseyi Joshua <iamoluwaseyiolasupo@gmail.com>
2023-10-20 05:31:18 +0000

    Update Filled.tsx

--- components/Button/Filled.tsx
+++ components/Button/Filled.tsx
@@ -1,22 +1,28 @@
 import { Button } from 'antd';
-import styled from 'styled-components';
 
 interface Props {
+  children: React.ReactNode;
   disabled?: boolean;
 }
 
-// Define a custom button component that wraps the Ant Design Button
-const FilledButton = styled(Button)<Props>`
-  background: ${(props) => props.disabled ? props.theme.spaceGrey : props.theme.primary + ' 0% 0% no-repeat padding-box'};
-  border: ${(props) => '2px solid ' + (props.disabled ? props.theme.grey : props.theme.primary)} !important;
-  color: ${(props) => props.disabled ? props.theme.grey : '#fff'} !important;
-  font-size: 1rem;
-  font-weight: bold;
-  padding: 0.5rem 1rem;
-  text-align: center;
-  text-transform: uppercase;
-  width: 100%;
-`;
+const Filled: React.FC<Props> = ({ children, disabled }) => {
+  return (
+    <Button
+      style={{
+        background: disabled ? '#BFBFBF' : '#1890ff',
+        border: '2px solid transparent',
+        borderRadius: '15px',
+        color: '#fff',
+        fontWeight: 'bold',
+        padding: '10px 35px',
+        textAlign: 'center',
+        height: '45px',
+      }}
+      disabled={disabled}
+    >
+      {children}
+    </Button>
+  );
+};
 
-// Export the styled custom button
-export default FilledButton;
+export default Filled;
